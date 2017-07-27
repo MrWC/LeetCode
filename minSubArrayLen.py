8653171e5fe4a227827e084e7c4f637b5fa91a21
MrWC <mrwcpr@gmail.com>
2017-07-27 15:21:28 +0800

    Create minSubArrayLen.py

---
+++ minSubArrayLen.py
@@ -0,0 +1,13 @@
+def minSubArrayLen(s, nums):
+        size = len(nums)
+        start, end, sum = 0, 0, 0
+        bestAns = size + 1
+        while end < size:
+            while end < size and sum < s:
+                sum += nums[end]
+                end += 1
+            while start < end and sum >= s:
+                bestAns = min(bestAns, end - start)
+                sum -= nums[start]
+                start += 1
+        return [0, bestAns][bestAns <= size]
